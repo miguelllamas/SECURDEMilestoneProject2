54c345f5226380499373ad290ff9a492219c6b8b
tadhg03 <tadhg_mccarthy@dlsu.edu.ph>
2019-04-09 15:03:30 +0800

    set clear button in logs to invisible

--- SECURDE MP Phase 2/src/View/MgmtLogs.java
+++ SECURDE MP Phase 2/src/View/MgmtLogs.java
@@ -26,7 +26,7 @@ public class MgmtLogs extends javax.swing.JPanel {
         table.getTableHeader().setFont(new java.awt.Font("SansSerif", java.awt.Font.BOLD, 14));
         
 //        UNCOMMENT TO DISABLE BUTTONS
-//        clearBtn.setVisible(false);
+        clearBtn.setVisible(false);
 //        debugBtn.setVisible(false);
     }
 
